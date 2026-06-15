5177b7007d044a95fb9d943171869de41f2fe7ac
Richard <rnegron@unsa.edu.pe>
2026-06-15 09:43:10 -0500

    pr 3

--- LAB 08/propuesto03.cpp
+++ LAB 08/propuesto03.cpp
@@ -1,72 +1,52 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
-
 using namespace std;
-
 // Nodo Trie
 class TrieNode {
 public:
-
     unordered_map<char, TrieNode*> children;
     bool isEnd;
-
-    TrieNode() {
-        isEnd = false;
-    }
+    TrieNode() { isEnd = false; }
 };
 
 // Trie
 class Trie {
 private:
     TrieNode* root;
-
 public:
 
-    Trie() {
-        root = new TrieNode();
-    }
+    Trie() { root = new TrieNode(); }
 
     void insert(string word) {
-
         TrieNode* current = root;
 
         for(char ch : word) {
-
             if(current->children.find(ch)
                 == current->children.end()) {
 
                 current->children[ch] =
                     new TrieNode();
             }
-
             current = current->children[ch];
         }
-
         current->isEnd = true;
     }
 
     bool search(string word) {
-
         TrieNode* current = root;
 
         for(char ch : word) {
-
             if(current->children.find(ch)
                 == current->children.end()) {
-
                 return false;
             }
-
             current = current->children[ch];
         }
-
         return current->isEnd;
     }
 };
-
 int main() {
-
     Trie trie;
 
     trie.insert("hello");
